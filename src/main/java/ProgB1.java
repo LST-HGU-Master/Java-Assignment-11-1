8448878fd911a799f6d7f52d116b1db1bf78a08f
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-07 16:21:42 +0900

    Update ProgB1.java

--- src/main/java/ProgB1.java
+++ src/main/java/ProgB1.java
@@ -1,12 +1,12 @@
 public class ProgB1 {
 
-	public static void main(String[] args) {
-		Hero h = new Hero();
-		h.name = "太郎";
-		h.hp = 100;
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.name = "太郎";
+        h.hp = 100;
 
-		System.out.println("勇者" + h.name + " (HP:" + h.hp + ") が誕生した!");
-	}
+        System.out.println("勇者" + h.name + " (HP:" + h.hp + ") が誕生した!");
+    }
 
 }
 
